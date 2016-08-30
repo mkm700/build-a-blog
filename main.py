8398a8f0078b84dfeb0323cb8abffe6b583a55dd
MK <murdaughmk@yahoo.com>
2016-08-29 19:43:31 -0500

    baseline working code (before adding hacking features)

--- main.py
+++ main.py
@@ -15,11 +15,71 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+from google.appengine.ext import db
 
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+#Create the Blog table in the DB
+class Blog(db.Model):
+    title = db.StringProperty(required = True)
+    post = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+#Displays a list of blog posts on the home page
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
-        self.response.write('Hello world!')
+        posts = db.GqlQuery("SELECT * FROM Blog "
+                            "ORDER BY created DESC "
+                            "LIMIT 5")
+
+        t = jinja_env.get_template("blog.html")
+        response = t.render(posts=posts)
+        self.response.write(response)
+
+
+#Add a new Blog Post
+class NewPostHandler(webapp2.RequestHandler):
+    def get(self):
+        t = jinja_env.get_template("newpost.html")
+        response = t.render()
+        self.response.write(response)
+
+    def post(self):
+        title = self.request.get("title")
+        post = self.request.get("post")
+
+        if title and post:
+            b = Blog(title = title, post = post)
+            b.put()
+            self.redirect(str(b.key().id()))
+        else:
+            error = "Please enter both a title and post text"
+            t = jinja_env.get_template("newpost.html")
+            response = t.render(title=title,post=post,error=error)
+            self.response.write(response)
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id):
+        id = int(id)
+        post = Blog.get_by_id(id)
+        if post:
+            t = jinja_env.get_template("single.html")
+            response = t.render(post=post,error="")
+            self.response.write(response)
+        else:
+            error = "Sorry... no blog exists with that ID."
+            t = jinja_env.get_template("single.html")
+            response = t.render(post=post,error=error)
+            self.response.write(response)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+#???How do I redirect the index page to /blog???
+#    ('/', MainHandler),
+    ('/blog', MainHandler),
+    ('/blog/newpost', NewPostHandler),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
